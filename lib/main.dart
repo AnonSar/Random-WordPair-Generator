de8a7145282a2f85a51a50470cde863b61ef27e6
ANMOL SARRAF <asar0011@student.monash.edu>
2020-02-05 20:54:25 +1100

    Uploading Application

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+          title: 'Startup Name Generator',
+          theme: ThemeData(          // Add the 3 lines from here...
+            primaryColor: Colors.red,
+                          ),
+          home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[]; // List for storing the suggested words
+  final Set<WordPair> _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0); // Variable for bigger fonts
+
+  // Adding the functionality to navigate to the another page
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(   // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  // Build method for constructing the word pairs
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[      // Adding interactivity to the list tiles
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  //
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+
+        // 1. Item builder propperty call the method for each pair suggestion and places them into the ListTile row
+        // 2. Adding a divider to the each row entry
+        // 3. This expression divides i by 2 and returns an integer result. This calculates the number of word pairing in the listview
+        // 4. If we have reached to the end of available word pairing, then generate 10 new word pairing
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider();
+          /*2*/
+
+          final index = i ~/ 2; /*3*/
+          print(index);
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+
+    final bool alreadySaved = _saved.contains(pair);  // Boolean expression to ensure whether a pair has been saved before or not
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(   // If the icon is already in favourite, then set the icon and it's color accordingly
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
